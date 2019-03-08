306c25c59d025127a2e494c64893ad02423de213
Brandon Murugan <brandonmurugan@gmail.com>
2019-03-08 17:44:18 +0200

    Updated documentation for data_preprocess function expanded and improved

--- R/data_preprocess.R
+++ R/data_preprocess.R
@@ -1,19 +1,19 @@
-#' Hello, world!
+#' Imports and preprocesses data files for label-free proteomic LC-MS experiments
 #'
-#' This is an example function named 'hello'
-#' which prints 'Hello, world!'.
+#' This function imports MaxQuant proteingroups.txt, peptides.txt, Phospho (STY)Sites.txt (if it exists), and mqpar.xml files.
+#' Proteingroups.txt file is then filtered and normalised according to chosen quantitation and normlisation types
 #'
-#' You can learn more about package authoring with RStudio at:
+#' @param txtpath MaxQuant txt folder
+#' @param mqparPath MaxQuant mqpar.xml file path
+#' @param quant_type Quanitiation type to select. Possible choices are "Intensity" (default), or "LFQ".
+#' @param norm_type Type of normalisation to perform. Possible choices are "MedianCentre" (default), "Quantile", or "None".
+#' @param valid_values Numeric. Removes entire protein if number of valid (non-zero) values are less than stated amount for \bold{any} group
+#' @param imputation Logical. If \code{TRUE}, will impute with lowest value of all intensities found in dataset. Default is FALSE
 #'
-#'   http://r-pkgs.had.co.nz/
 #'
-#' Some useful keyboard shortcuts for package authoring:
-#'
-#'   Build and Reload Package:  'Ctrl + Shift + B'
-#'   Check Package:             'Ctrl + Shift + E'
-#'   Test Package:              'Ctrl + Shift + T'
+#' @author Brandon D. Murugan
 
-data_preprocess <- function(txtfolder = txtpath, mqparPath = mqpath, Quantitation = QuantType){
+data_preprocess <- function(txtpath, mqparfile, quant_type = "Intensity", norm_type = "MedianCentre", valid_values = 3, imputation = F){
   library(grid)
   library(XML)
   library(xml2)
@@ -27,9 +27,12 @@ data_preprocess <- function(txtfolder = txtpath, mqparPath = mqpath, Quantitatio
 
   direct <- txtpath
 
+  # direct <- "C:/Users/Brandon/Documents/Data Analysis/Kim_reprocess/H_timecourse"
+  # load(paste0(direct,"/images/imported_Data.RData"))
+
   # Rip info from mqpar file (also moved into text folder; less work on user's part) ####
           # mqpar <- xmlTreeParse(file = paste0(direct,"//mqpar.xml"))
-  mqpar <- xmlTreeParse(file = mqpath)
+  mqpar <- xmlTreeParse(file = mqparfile)
   # mqpar <- read_xml(paste0(direct,"\\mqpar.xml"))
   topMqpar <- xmlRoot(mqpar)
   # topMqpar <- xml_root(mqpar)
@@ -66,7 +69,7 @@ data_preprocess <- function(txtfolder = txtpath, mqparPath = mqpath, Quantitatio
 
   # txtpath <- "C:/Users/Brandon/Documents/Data Analysis/Kim_reprocess/H_timecourse"
 
-  load(paste0(direct,"/images/imported_Data.RData"))
+  # load(paste0(direct,"/images/imported_Data.RData"))
 
   # Remove Contaminants, Reverse hits ####
   # proteinGroups <- read.delim("~/example_txt/proteinGroups.txt", sep = "\t")
@@ -80,17 +83,53 @@ data_preprocess <- function(txtfolder = txtpath, mqparPath = mqpath, Quantitatio
   uniprot.IDs <- data.frame(uniprot.IDs[,1])
   colnames(uniprot.IDs) <- "Protein.ID"
 
-  if (QuantType == "Intensity"){
+  if (quant_type == "Intensity"){
     intensity.PG <- cbind(uniprot.IDs, PGs[grep(colnames(PGs), pattern = "Intensity.")])
     intensity.PG$Protein.ID <- as.character(intensity.PG$Protein.ID)
     # Make quant file with common name despite LFQ/Intensity (makes downstream tasks easier)
     ProteinQuant <- intensity.PG
   }
 
-  if (QuantType == "LFQ"){
+  if (quant_type == "LFQ"){
     LFQ.PG <- cbind(uniprot.IDs, PGs[grep(colnames(PGs), pattern = "LFQ.")])
     LFQ.PG$Protein.ID <- as.character(LFQ.PG$Protein.ID)
     # Make quant file with common name despite LFQ/Intensity (makes downstream tasks easier)
     ProteinQuant <- LFQ.PG
   }
+  # Normalise data (usually for Intensity data, Choose in python GUI**) ####
+  if (norm_type == "MedianCentre"){
+    ProteinQuant[ProteinQuant == 0] <- NA
+    ProteinQuant.norm <- ProteinQuant[,-1]
+    ColumnMedians <- colMedians(ProteinQuant.norm, na.rm = T)
+    ColumnMedians <- as.data.frame(ColumnMedians)
+    meanMeans <- colMeans(ColumnMedians)
+    normFactor <- meanMeans/ColumnMedians
+    normFactor <- t(normFactor)
+    ProteinQuant.norm <- data.frame(mapply("*", ProteinQuant.norm, normFactor))
+    ProteinQuant.norm <- cbind(Protein.ID = ProteinQuant[,1], ProteinQuant.norm)
+  }
+
+  if (norm_type == "None"){
+    ProteinQuant.norm <- ProteinQuant
+  }
+
+  if (norm_type == "Quantile"){
+    ProteinQuant[ProteinQuant == 0] <- NA
+    ProteinQuant.norm <- normalize.quantiles(as.matrix(ProteinQuant[,-1]))
+    ProteinQuant.norm <- as.data.frame(ProteinQuant.norm)
+    ProteinQuant.norm <- cbind(ProteinQuant[,1], ProteinQuant.norm)
+    # colnames(ProteinQuant.norm) <- colnames(ProteinQuant)
+  }
+
+  load("ProteinQuant.norm.rda")
+
+  # # VALID VALUE HANDLING
+  #   ProteinQuant.norm <- ProteinQuant.norm[complete.cases(ProteinQuant.norm) ,]
+
+  # IMPUTATION 9IN PROGRESS
+  for (i in colnames(ProteinQuant.norm[,-1])){
+    ProteinQuant.norm[is.na(ProteinQuant.norm),i] <- min(ProteinQuant.norm[,i], na.rm = T)}
+
+
+
 }
