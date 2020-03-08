0723ca9c29887842c65cf861e7a4b98e9bb3064b
Brandon Murugan <brandonmurugan@gmail.com>
2020-03-08 10:20:21 -0700

    Added uniprot mapping data file (uniprotKB to GeneName for reviewed entries only)

---
+++ R/UniprotMappingData.R
@@ -0,0 +1,21 @@
+#' UniProt Human GeneName to UniProtKB mapping file
+#'
+#' UniProt Human Reference proteome filtered for SwissProt entries only (Reviewed)
+#' Data extracted on 24 Feb 2020
+#'
+#' @docType data
+#'
+#' @usage data(uniprot.mapping)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords uniprot
+#'
+#' @source \href{https://www.uniprot.org}{UniProtKB}
+#'
+#' @examples
+#' data(grav)
+#' times <- attr(grav, "time")
+#' phe <- grav$pheno
+#' \donttest{iplotCurves(phe, times)}
+"Uniprot2Genename_swissprot_20200224"
