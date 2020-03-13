9e743c5156d5cc8cfcdf2aabf56e65dce5c4495d
Brandon Murugan <brandonmurugan@gmail.com>
2020-03-12 21:33:20 -0700

    fixed gene.name.extractor added entrez mapping included NCBI genome 20200210

---
+++ R/NCBIMappingData.R
@@ -0,0 +1,18 @@
+#' NCBI Human mapping file
+#'
+#' NCBI data for human genome (GRCh38.p13)
+#' Genome Reference Consortium (March 2019)
+#' Data extracted on 10 February 2020
+#'
+#' @docType data
+#'
+#' @usage data(NCBI_200210)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords NCBI
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/gene}{NCBI}
+#'
+#' @examples None
+"NCBI_200210"
--- R/gene.name.extractor.R
+++ R/gene.name.extractor.R
@@ -12,45 +12,62 @@
 #' @author Brandon D. Murugan
 
 gene.name.extractor <- function(inputDF, SwissProt.only = T){
-  library(EnsDb.Hsapiens.v86)
+  # library(EnsDb.Hsapiens.v86)
   data(Uniprot2Genename_swissprot_20200224)
+  data(NCBI_200210)
   # library(SetRank)
   # source('~/Data Analysis/proteomestats/R/createIDconverter2.R')
-  inputDF$Fasta.headers <- sub(";.*$","",inputDF$Fasta.headers)
+  inputDF <- sp.select(input.df = inputDF, ProteinID.column = "Fasta.headers", new.ID.column = "Protein.ID_spFilter", delimiter = ";")
   rownames(inputDF) <- inputDF$seqid <- seq(1,length(inputDF$Fasta.headers))
+  # inputDF$Fasta.headers <- sub(";.*$","",inputDF$Fasta.headers)
   unkGene.names <- inputDF
-  unkGene.names <- data.frame(GeneNames = unkGene.names$Fasta.headers, seqid=unkGene.names$seqid, stringsAsFactors = F)
+  unkGene.names <- data.frame(GeneNames = unkGene.names$Protein.ID_spFilter, GeneName_MQ = unkGene.names$Gene.names, seqid=unkGene.names$seqid, stringsAsFactors = F)
   unkGene.names$GeneNames <- as.character(unkGene.names$GeneNames)
+  unkGene.names$GeneName_MQ <- as.character(unkGene.names$GeneName_MQ)
 
-  GN <- unkGene.names[grep("GN=[[:alnum:]]+",unkGene.names$GeneNames),]
-  rest <- setdiff(unkGene.names$seqid,GN$seqid)
-  nonGN <- unkGene.names[unkGene.names$seqid %in% rest,]
-
+  # GN from Fasta Header
+  GN <- unkGene.names[grep("PE=[[:alnum:]]+",unkGene.names$GeneNames),]  #Fasta header until PE at least
+  GN <- GN[grep("GN=[[:alnum:]]+",GN$GeneNames),]
   GN$GeneNames <- sub("(^.*.GN=)","",GN$GeneNames)
   GN$GeneNames <- sub("( PE.*.$)","",GN$GeneNames)
-  nonGN$GeneNames <- sub("^sp\\|.*\\|","",nonGN$GeneNames)
-  nonGN$GeneNames <- sub("^tr\\|.*\\|","",nonGN$GeneNames)
-  nonGN$GeneNames <- sub("_HU.*$","",nonGN$GeneNames)
-  uniprot2symbol <- proteomestats::createIDconverter2(annotationPackageName = "EnsDb.Hsapiens.v86", from = "UNIPROTID", to = "SYMBOL")
-  nonGN$GeneNames <- uniprot2symbol(nonGN$GeneNames, na.rm=FALSE, drop.ambiguous=TRUE)
-  GeneNames_2 <- rbind(GN,nonGN)
 
-  temp <- inputDF[inputDF$seqid %in% GeneNames_2$seqid,]
-  inputDF_2 <- inputDF[!inputDF$seqid %in% GeneNames_2$seqid,]
-  temp$Gene.names <- NULL
+  # from MQ genename column
+  rest <- setdiff(unkGene.names$seqid,GN$seqid)
+  nonGN <- unkGene.names[unkGene.names$seqid %in% rest,]
+  nonGN$GeneName_MQ <- sub(";.*$","",nonGN$GeneName_MQ)
+  nonGN$GeneNames <- nonGN$GeneName_MQ
 
+  GeneNames_2 <- rbind(GN, nonGN)
+  # temp <- inputDF[inputDF$seqid %in% GeneNames_2$seqid,]
+  # inputDF_2 <- inputDF[!inputDF$seqid %in% GeneNames_2$seqid,]
   temp <- dplyr::inner_join(GeneNames_2,temp, by = "seqid")
+  temp$Gene.names <- NULL
   colnames(temp)[1] <- "Gene.names"
-  temp <- temp[,colnames(inputDF_2)]
-  inputDF_2 <- rbind(inputDF_2,temp)
+  inputDF_2 <- temp
   inputDF_2 <- inputDF_2[!is.na(inputDF_2$Gene.names),]
-  inputDF_2$Gene.names <- sub(";.*$","",inputDF_2$Gene.names)
+  # inputDF_2$Gene.names <- sub(";.*$","",inputDF_2$Gene.names)
   inputDF_2$seqid <- NULL
-  inputDF_2$uniprot <- sub("^(tr|sp)\\|","",inputDF_2$Fasta.headers)
+  inputDF_2$uniprot <- sub("^(tr|sp)\\|","",inputDF_2$Protein.ID_spFilter)
   inputDF_2$uniprot <- sub("\\|.*$","",inputDF_2$uniprot)
   if (!is.null(inputDF_2$id)) inputDF_2$id <- as.numeric(inputDF_2$id)
 
-  inputDF_2 <- dplyr::left_join(inputDF_2, Uniprot2Genename_swissprot_20200224, "Gene.names")
+  # Sort out blanks
+  inputDF_2[grep("^$",inputDF_2$Gene.names),"Gene.names"] <- inputDF_2[grep("^$",inputDF_2$Gene.names),"uniprot"]
+  inputDF_2[grep("^$",inputDF_2$Gene.names),"GeneName_MQ"] <- inputDF_2[grep("^$",inputDF_2$Gene.names),"uniprot"]
+
+  # Adding reviewed uniprot IDs from genenames
+  inputDF_2 <- dplyr::left_join(inputDF_2, Uniprot2Genename_swissprot_20200224[,c("Gene.names","Entry")], "Gene.names")
+  inputDF_2$Entry <- as.character(inputDF_2$Entry)
+  inputDF_2$Entry[is.na(inputDF_2$Entry)] <- inputDF_2$uniprot[is.na(inputDF_2$Entry)]
+
+
+  # Add ENTREZID from NCBI200210
+  colnames(NCBI_200210)[colnames(NCBI_200210) == "Symbol"] <- "Gene.names"
+  inputDF_2 <- dplyr::left_join(inputDF_2, NCBI_200210[,c("Gene.names", "GeneID")], "Gene.names")
+
+  # uniprot2entrez <- proteomestats::createIDconverter2(annotationPackageName = "EnsDb.Hsapiens.v86", from = "UNIPROTID", to = "ENTREZID")
+  # nonGN$GeneNames <- uniprot2symbol(nonGN$GeneNames, na.rm=FALSE, drop.ambiguous=TRUE)
+
   colnames(inputDF_2)[colnames(inputDF_2)=="Entry"] <- "UniProt_sp"
   return(inputDF_2)
 }
