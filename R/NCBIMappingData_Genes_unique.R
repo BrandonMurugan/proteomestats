2338b7e45397cbf5147f5f2a3686263862717394
Brandon Murugan <brandonmurugan@gmail.com>
2020-03-14 15:26:47 -0700

    Filtered NCBI_genes_200210 for unique genes

---
+++ R/NCBIMappingData_Genes_unique.R
@@ -0,0 +1,18 @@
+#' NCBI Human mapping file - Genes: Filtered
+#'
+#' NCBI Genes
+#' Data extracted on 10 Feb 2020
+#' Duplicates removed
+#'
+#' @docType data
+#'
+#' @usage data(NCBI_genes_200210_unique)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords NCBI
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/gene}{NCBI}
+#'
+#' @examples None
+"NCBI_genes_200210_unique"
