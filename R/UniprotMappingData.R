71fcabacdc5c6973bc5e5e45dd7d526651d8b6cc
Brandon Murugan <brandonmurugan@gmail.com>
2020-03-13 22:37:00 -0700

    updated uniprot mapping file with GeneID

--- R/UniprotMappingData.R
+++ R/UniprotMappingData.R
@@ -1,11 +1,11 @@
 #' UniProt Human GeneName to UniProtKB mapping file
 #'
 #' UniProt Human Reference proteome filtered for SwissProt entries only (Reviewed)
-#' Data extracted on 24 Feb 2020
+#' Data extracted on 13 March 2020
 #'
 #' @docType data
 #'
-#' @usage data(Uniprot2Genename_swissprot_20200224)
+#' @usage data(Uniprot2Genename_swissprot_20200313)
 #'
 #' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
 #'
@@ -14,4 +14,4 @@
 #' @source \href{https://www.uniprot.org}{UniProtKB}
 #'
 #' @examples None
-"Uniprot2Genename_swissprot_20200224"
+"Uniprot2Genename_swissprot_20200313"
