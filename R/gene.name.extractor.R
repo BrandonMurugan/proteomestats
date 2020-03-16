6586de77b0e94a98239097cc692564902a1d9c63
Brandon Murugan <brandonmurugan@gmail.com>
2020-03-15 21:16:03 -0700

    mod gene.name.extractor

--- R/gene.name.extractor.R
+++ R/gene.name.extractor.R
@@ -13,8 +13,10 @@
 
 gene.name.extractor <- function(inputDF, SwissProt.only = T){
   # library(EnsDb.Hsapiens.v86)
-  data(Uniprot2Genename_swissprot_20200224)
-  data(NCBI_200210)
+  library(proteomestats)
+  data(Uniprot2Genename_swissprot_20200313)
+  # data(NCBI_assembly_200313)
+  data(NCBI_genes_200210)
   # library(SetRank)
   # source('~/Data Analysis/proteomestats/R/createIDconverter2.R')
   inputDF <- sp.select(input.df = inputDF, ProteinID.column = "Fasta.headers", new.ID.column = "Protein.ID_spFilter", delimiter = ";")
@@ -40,7 +42,7 @@ gene.name.extractor <- function(inputDF, SwissProt.only = T){
   GeneNames_2 <- rbind(GN, nonGN)
   # temp <- inputDF[inputDF$seqid %in% GeneNames_2$seqid,]
   # inputDF_2 <- inputDF[!inputDF$seqid %in% GeneNames_2$seqid,]
-  temp <- dplyr::inner_join(GeneNames_2,temp, by = "seqid")
+  temp <- dplyr::inner_join(GeneNames_2, inputDF, by = "seqid")
   temp$Gene.names <- NULL
   colnames(temp)[1] <- "Gene.names"
   inputDF_2 <- temp
@@ -55,18 +57,43 @@ gene.name.extractor <- function(inputDF, SwissProt.only = T){
   inputDF_2[grep("^$",inputDF_2$Gene.names),"Gene.names"] <- inputDF_2[grep("^$",inputDF_2$Gene.names),"uniprot"]
   inputDF_2[grep("^$",inputDF_2$Gene.names),"GeneName_MQ"] <- inputDF_2[grep("^$",inputDF_2$Gene.names),"uniprot"]
 
+  inputDF_2 <- dplyr::left_join(inputDF_2, Uniprot2Genename_swissprot_20200313[,c("Gene.names","Entry")], "Gene.names")
+
   # Adding reviewed uniprot IDs from genenames
-  inputDF_2 <- dplyr::left_join(inputDF_2, Uniprot2Genename_swissprot_20200224[,c("Gene.names","Entry")], "Gene.names")
+  inputDF_2 <- dplyr::left_join(inputDF_2, Uniprot2Genename_swissprot_20200313[,c("Gene.names","Entry")], "Gene.names")
   inputDF_2$Entry <- as.character(inputDF_2$Entry)
   inputDF_2$Entry[is.na(inputDF_2$Entry)] <- inputDF_2$uniprot[is.na(inputDF_2$Entry)]
 
+  # # Add ENTREZID from NCBI_200210
+  # colnames(NCBI_genes_200210_unique)[colnames(NCBI_genes_200210_unique) == "Symbol"] <- "Gene.names"
+  # inputDF_3 <- dplyr::left_join(inputDF_2, NCBI_genes_200210_unique[,c("Gene.names", "GeneID")], "Gene.names")
+  # noEntrez <- inputDF_3[is.na(inputDF_3$GeneID),]
+  # inputDF_3 <- inputDF_3[!is.na(inputDF_3$GeneID),]
+  #
+  # symbol2entrez <- proteomestats::createIDconverter2(annotationPackageName = "EnsDb.Hsapiens.v86", from = "SYMBOL", to = "ENTREZID")
+  # noEntrez$GeneID <- symbol2entrez(noEntrez$Gene.names, na.rm=FALSE, drop.ambiguous=TRUE)
+  # noEntrez_2 <- noEntrez[is.na(noEntrez$GeneID),]
+  # noEntrez <- noEntrez[!is.na(noEntrez$GeneID),]
+  #
+  #
+  # noEntrez_2$GeneID <- NULL
+  # # Add ENTREZID from uniprot_mapping
+  # noEntrez_2 <- dplyr::left_join(noEntrez_2, Uniprot2Genename_swissprot_20200313[,c("Entry", "Cross.reference..GeneID.")], "Entry")
+  # colnames(noEntrez_2)[colnames(noEntrez_2) == "Cross.reference..GeneID."] <- "GeneID"
+  # noEntrez_2$GeneID <- as.character(noEntrez_2$GeneID)
+  # noEntrez_2$GeneID <- sub(";","",noEntrez_2$GeneID)
+  # noEntrez_2$GeneID <- sub("^$",NA,noEntrez_2$GeneID)
+  # noEntrez_3 <- noEntrez_2[is.na(noEntrez_2$GeneID),]
+  # noEntrez_2 <- noEntrez_2[!is.na(noEntrez_2$GeneID),]
+  #
 
-  # Add ENTREZID from NCBI200210
-  colnames(NCBI_200210)[colnames(NCBI_200210) == "Symbol"] <- "Gene.names"
-  inputDF_2 <- dplyr::left_join(inputDF_2, NCBI_200210[,c("Gene.names", "GeneID")], "Gene.names")
 
-  # uniprot2entrez <- proteomestats::createIDconverter2(annotationPackageName = "EnsDb.Hsapiens.v86", from = "UNIPROTID", to = "ENTREZID")
-  # nonGN$GeneNames <- uniprot2symbol(nonGN$GeneNames, na.rm=FALSE, drop.ambiguous=TRUE)
+  # colnames(noEntrez)[colnames(noEntrez)=="Aliases"] <- "Gene.names"
+  # colnames(noEntrez_2)[colnames(noEntrez_2)=="Aliases"] <- "Gene.names"
+  #
+  # inputDF_4 <- rbind(inputDF_3, noEntrez, noEntrez_2)
+
+
 
   colnames(inputDF_2)[colnames(inputDF_2)=="Entry"] <- "UniProt_sp"
   return(inputDF_2)
